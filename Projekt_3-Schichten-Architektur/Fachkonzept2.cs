c413202c79e491757d251c1013af114eab03290d
chukarave <noa.rave@berlinonline.de>
2017-10-27 14:47:36 +0200

    Fachkonzept2

--- Projekt_3-Schichten-Architektur/Fachkonzept2.cs
+++ Projekt_3-Schichten-Architektur/Fachkonzept2.cs
@@ -12,46 +12,70 @@ namespace Projekt_3_Schichten_Architektur
         public Fachkonzept2(IDatenhaltung Data)
         {
             this.Data = Data;
+            
         }
 
-		public void AktualisiereAutor(int ID, string Name)
-		{
-			throw new NotImplementedException();
-		}
+        public void AktualisiereAutor(int ID, string Name)
+        {
+            Data.AktualisiereAutor(ID, Name);
+        }
 
-		public void AktualisiereBuch(string ISBN, string Titel)
-		{
-			throw new NotImplementedException();
-		}
+        public void AktualisiereBuch(string ISBN, string Titel)
+        {
+            throw new NotImplementedException();
+        }
 
-		public List<Autor> GetAutoren()
-		{
-			throw new NotImplementedException();
-		}
+        public List<Autor> GetAutoren()
+        {
+            return SortiereAutoren(Data.GetAutoren());
+        }
 
-		public List<Buch> GetBuecher(int Autoren_id)
-		{
-			throw new NotImplementedException();
-		}
+        public List<Buch> GetBuecher(int autorenId)
+        {
+            throw new NotImplementedException();
+        }
 
-		public void LoescheAutor(int ID)
-		{
-			throw new NotImplementedException();
-		}
+        public void LoescheAutor(int ID)
+        {
+            throw new NotImplementedException();
+        }
 
-		public void LoescheBuch(string ISBN)
-		{
-			throw new NotImplementedException();
-		}
+        public void LoescheBuch(string ISBN)
+        {
+            throw new NotImplementedException();
+        }
 
-		public void SpeichereAutor(string Name)
-		{
-			throw new NotImplementedException();
-		}
+        public void SpeichereAutor(string Name)
+        {
+            throw new NotImplementedException();
+        }
 
-		public void SpeichereBuch(int Autoren_id, string ISBN, string Titel)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public void SpeichereBuch(int autorenId, string ISBN, string Titel)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private List<Autor> SortiereAutoren(List<Autor> Liste)
+        {
+            string abc = "aäbcdefghijklmnoöpqrstuüvwxyz";
+
+            for (int i = 0; i < Liste.Count; i++)
+            {
+                int index = abc.IndexOf(Liste[i].Name.ToLower().ToCharArray()[0]);
+
+                for (int j = Liste.Count - 1; j > i; j--)
+                {
+                    if (index > abc.IndexOf(Liste[j].Name.ToLower().ToCharArray()[0]))
+                    {
+                        Autor tempAutor = Liste[j];
+                        Liste[j] = Liste[i];
+                        Liste[i] = tempAutor;
+                        i--;
+                        break;
+                    }
+                }
+            }
+            return Liste;
+        }
+    }
 }
