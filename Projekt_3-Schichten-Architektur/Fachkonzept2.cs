10c0cebbcb5562e1669d8b6753200aff1bf6d48c
chukarave <noa.rave@berlinonline.de>
2017-11-08 14:37:19 +0100

    fix namespace

--- Projekt_3-Schichten-Architektur/Fachkonzept2.cs
+++ Projekt_3-Schichten-Architektur/Fachkonzept2.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AsProject
+namespace Projekt_3_Schichten_Architektur
 {
     public class Fachkonzept2 : IFachkonzept
     {
