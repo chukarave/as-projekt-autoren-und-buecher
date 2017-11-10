ae9c442bb8efdfc5cb0d54b2bba0cb1fff08ab4c
chukarave <noa.rave@berlinonline.de>
2017-11-10 14:43:47 +0100

    add Exception class

---
+++ Projekt_3-Schichten-Architektur/DatenhaltungsFehler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projekt_3_Schichten_Architektur
+{
+    public class DatenhaltungsFehler : Exception
+    {
+        public DatenhaltungsFehler(string nachricht) : base(nachricht) 
+        {
+        }
+        
+
+    }
+}
